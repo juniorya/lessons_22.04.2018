9e45de51bbd72950ac4acebfa614808fae180ac1
juniorya <38587101+juniorya@users.noreply.github.com>
2018-04-21 17:48:53 +0300

    Add files via upload

---
+++ SERIAL_PRINT_LED/SERIAL_PRINT_LED.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(115200);
+  //pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  while (Serial.available() != 0) {
+    Serial.println("Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, ");
+    delay(100);
+  }
+//  else {
+//    //    digitalWrite(LED_BUILTIN, HIGH);
+//    //    delay(100);
+//    //    digitalWrite(LED_BUILTIN, LOW);
+//    //    delay(100);
+//  }
+}
