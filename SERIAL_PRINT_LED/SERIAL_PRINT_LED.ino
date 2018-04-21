4bf8debce41a07582b9c236fa82f7935ac5af2a2
juniorya <38587101+juniorya@users.noreply.github.com>
2018-04-21 17:54:01 +0300

    Update SERIAL_PRINT_LED.ino

--- SERIAL_PRINT_LED/SERIAL_PRINT_LED.ino
+++ SERIAL_PRINT_LED/SERIAL_PRINT_LED.ino
@@ -5,13 +5,7 @@ void setup() {
 
 void loop() {
   while (Serial.available() != 0) {
-    Serial.println("Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, Hello, World!, ");
+    Serial.println("Hello, World!");
     delay(100);
   }
-//  else {
-//    //    digitalWrite(LED_BUILTIN, HIGH);
-//    //    delay(100);
-//    //    digitalWrite(LED_BUILTIN, LOW);
-//    //    delay(100);
-//  }
 }
