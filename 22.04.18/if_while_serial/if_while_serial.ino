291e76149d26989e19f6e5b4d78c3bc562ca05e0
juniorya <38587101+juniorya@users.noreply.github.com>
2018-04-21 19:06:20 +0300

    причесал

--- 22.04.18/if_while_serial/if_while_serial.ino
+++ 22.04.18/if_while_serial/if_while_serial.ino
@@ -1,19 +1,19 @@
 
-int LED=1;
+int LED = 1;
 void setup() {
   Serial.begin(115200);                 //Начинаем передачу на скорости 115200
-  pinMode(LED, OUTPUT);       //№ ножки к которой подключен диод и выход
+  pinMode(LED, OUTPUT);                 //№ ножки к которой подключен диод и выход
 }
 
 void loop() {
   if (Serial.available() != 0) {        //Если есть подключение по SERIAL, то
-    Serial.println("Hello, World!");
-    delay(100);
+    Serial.println("Hello, World!");    //Запись в порт Hello, World!
+    delay(100);                         //задержка - 100 мс
   }
   else {
-    digitalWrite(LED,HIGH);
-    delay(100);
-    digitalWrite(LED,LOW);
-    delay(100);
-    }
+    digitalWrite(LED, HIGH);            //Включаем светодиод
+    delay(100);                         //задержка - 100 мс
+    digitalWrite(LED, LOW);             //Выключаем светодиод
+    delay(100);                         //задержка - 100 мс
+  }
 }
