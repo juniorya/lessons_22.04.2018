8b8c48b6dfa9c8db6d7d2903833d403304d83344
Mikhail Golyakov <juniorya@mail.ru>
2019-03-24 15:03:01 +0300

    add

--- nano_test/src/main.cpp
+++ nano_test/src/main.cpp
@@ -1,17 +1,20 @@
 #include <Arduino.h>
 
-void setup() {
- Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  int i=1;
-  for(i = 0; i < 50; i++)
-  {
+void loop(){
+  /*{
+  int i = 1;
+  for (i = 0; i < 50; i++)
   Serial.print(i);
-  Serial.print("Hello_World");
-  //Serial.print('\n');
-  delay(100);
+    Serial.print("Hello_World");
+
+    delay(100);
   }
-  exit(0);
+  exit(0);*/
+Serial.println("|-|");
+delay(1000);
 }
