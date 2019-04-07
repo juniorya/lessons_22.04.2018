8880c4ff713f4981f226814d2d6039f1908327b5
Mikhail Golyakov <juniorya@mail.ru>
2019-04-07 12:56:43 +0300

    add

--- 31.03.19/31.03.19/src/main.cpp
+++ 31.03.19/31.03.19/src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(7,HIGH);
+  delay(1000);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+  delay(1000);
+  digitalWrite(8,LOW);
 }
