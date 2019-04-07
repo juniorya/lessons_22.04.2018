0a43a409fac75f3e1b18d70847d490d4f8f522e4
Mikhail Golyakov <juniorya@mail.ru>
2019-04-07 14:59:06 +0300

    add_serial print state of led

--- led_serial/src/main.cpp
+++ led_serial/src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN,HIGH);
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED is ON");
   delay(500);
-  digitalWrite(LED_BUILTIN,LOW);
-  delay(500); 
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED is OFF");/**/
+  delay(500);
 }
